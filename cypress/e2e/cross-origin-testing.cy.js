fa99195f5e35ec31a9ddc588305ba82eb5ac4244
aashrikaa <aashrikaa.chand@bajratechnologies.com>
2025-12-19 10:50:59 +0545

    feat: testing ci

--- cypress/e2e/cross-origin-testing.cy.js
+++ cypress/e2e/cross-origin-testing.cy.js
@@ -2,7 +2,7 @@ it("Basic cross-origin test", () => {
   cy.visit("https://w3schools.com");
 
   cy.origin("https://google.com", () => {
-    cy.visit("/");
+    cy.visit("");
     cy.title().should("include", "Google");
   });
 });
