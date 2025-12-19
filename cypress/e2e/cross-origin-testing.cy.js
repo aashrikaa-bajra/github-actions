4bde586990a0bda9030bdd615710ad202667196b
aashrikaa <aashrikaa.chand@bajratechnologies.com>
2025-12-19 10:33:20 +0545

    feat: add .yml file for CI

---
+++ cypress/e2e/cross-origin-testing.cy.js
@@ -0,0 +1,8 @@
+it("Basic cross-origin test", () => {
+  cy.visit("https://w3schools.com");
+
+  cy.origin("https://google.com", () => {
+    cy.visit("/");
+    cy.title().should("include", "Google");
+  });
+});
